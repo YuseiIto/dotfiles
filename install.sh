ada2e1df18ad8f0b5c1f9f01245a8ae3450efc00
Yusei Ito <me@yuseiito.com>
2022-07-17 03:34:14 +0900

    Add: Git configuration

--- install.sh
+++ install.sh
@@ -7,5 +7,7 @@ ln -fns $DOTFILES_DIR/.zshrc ~/.zshrc
 ln -fns $DOTFILES_DIR/.config/nvim ~/.config/nvim
 ln -fns $DOTFILES_DIR/.hyper.js ~/.hyper.js 
 ln -fns $DOTFILES_DIR/.tmux.conf ~/.tmux.conf
+ln -fns $DOTFILES_DIR/.config/git ~/.config/git
+
 ln -fns $DOTFILES_DIR/Brewfile ~/Brewfile
 brew bundle

58a0e05740ab0f0a826d1949575e5608de02d8a3
Yusei Ito <me@yuseiito.com>
2022-07-17 03:02:49 +0900

    Add: Brewfile

--- install.sh
+++ install.sh
@@ -7,3 +7,4 @@ ln -fns $DOTFILES_DIR/.zshrc ~/.zshrc
 ln -fns $DOTFILES_DIR/.config/nvim ~/.config/nvim
 ln -fns $DOTFILES_DIR/.hyper.js ~/.hyper.js 
 ln -fns $DOTFILES_DIR/.tmux.conf ~/.tmux.conf
+ln -fns $DOTFILES_DIR/Brewfile ~/Brewfile

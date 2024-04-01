29151a140c283d2c5ab7fce6c1ec3fe8360e280e
Yusei Ito <me@yuseiito.com>
2024-04-01 15:26:48 +0900

    fmt: 余計なspaceを消す

--- .config/nvim/lua/lazygit.lua
+++ .config/nvim/lua/lazygit.lua
@@ -1,6 +1,6 @@
 local Terminal  = require('toggleterm.terminal').Terminal
-local lazygit = Terminal:new({ 
-  cmd = "lazygit", 
+local lazygit = Terminal:new({
+  cmd = "lazygit",
   hidden = true,
   direction = "float",
 })

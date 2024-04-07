549a0beb998c403641e27b089629b9321bf046c2
Yusei Ito <me@yuseiito.com>
2024-04-07 18:24:16 +0900

    fix: install scriptをアップデート

--- install.sh
+++ install.sh
@@ -6,24 +6,17 @@ DOTFILES_DIR=$(cd $(dirname $0); pwd)
 echo $DOTFILES_DIR
 ln -fns $DOTFILES_DIR/.zshrc ~/.zshrc
 ln -fns $DOTFILES_DIR/.config/nvim ~/.config/nvim
-ln -fns $DOTFILES_DIR/.hyper.js ~/.hyper.js 
 ln -fns $DOTFILES_DIR/.tmux.conf ~/.tmux.conf
 ln -fns $DOTFILES_DIR/.config/git ~/.config/git
 ln -fns $DOTFILES_DIR/.tmux ~/.tmux
 
-
 cd $DOTFILES_DIR && git submodule init && git submodule update
-cd $DOTFILES_DIR/powerline-shell && python3 setup.py install
 
 if [[ $OSTYPE == 'darwin'* ]]; then
 	ln -fns $DOTFILES_DIR/Brewfile ~/Brewfile
-	brew bundle
+#	brew bundle
 fi
 
 if [[ $OSTYPE == 'linux-gnu'* ]]; then
 	source $DOTFILES_DIR/install_linux.sh
 fi
-
-
-# Install Poetry. This line is placed after package installation because it requires python
-curl -sSL https://install.python-poetry.org | python3 -

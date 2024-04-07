407f5b9c63a504e4d34a32d621a2780c52cc3728
Yusei Ito <me@yuseiito.com>
2024-04-07 21:31:53 +0900

    feat(nix): denoやrg等をnixで入れる

--- home/darwin/default.nix
+++ home/darwin/default.nix
@@ -1,6 +1,7 @@
 { pkgs, ... }: {
   home.stateVersion = "23.11";
-  home.packages = with pkgs; [ discord bat eza ocaml ];
+  home.packages = with pkgs; [ discord bat eza gnumake R ccls cloc coq ocaml procs hexyl ripgrep deno jq
+];
 
   programs = { home-manager.enable = true; };
 }

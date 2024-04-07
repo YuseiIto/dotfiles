3231a19a314f7389b2477cabd85d0b1ab0d2d02d
Yusei Ito <me@yuseiito.com>
2024-04-07 22:06:19 +0900

    feat(nix): Install gimp, inkscape etc...

--- home/darwin/default.nix
+++ home/darwin/default.nix
@@ -1,7 +1,36 @@
 { pkgs, ... }: {
   home.stateVersion = "23.11";
-  home.packages = with pkgs; [ discord bat eza gnumake R ccls cloc coq ocaml procs hexyl ripgrep deno jq
-];
+  home.packages = with pkgs; [
+    discord
+    bat
+    eza
+    gnumake
+    R
+    ccls
+    cloc
+    coq
+    ocaml
+    procs
+    hexyl
+    ripgrep
+    deno
+    jq
+    utm
+    gimp
+    drawio
+    inkscape
+    yq-go
+    aws-sam-cli
+    firebase-tools
+    go
+    lazygit
+  ];
 
-  programs = { home-manager.enable = true; };
+  programs = {
+    home-manager.enable = true;
+    direnv = {
+      enable = true;
+      enableZshIntegration = true;
+    };
+  };
 }

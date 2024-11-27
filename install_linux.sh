3cbad6e12e6f76b6a467dc320e06a6fb875240f2
Yusei Ito <me@yuseiito.com>
2024-11-27 20:17:12 +0900

    fix: cleanup install_linux.sh

--- install_linux.sh
+++ install_linux.sh
@@ -2,30 +2,9 @@
 
 sudo apt update
 
-# setup anyenv
-ln -fsn $DOTFILES_DIR/anyenv ~/.anyenv
-sudo apt install python3-pip
-cd $DOTFILES_DIR/anyenv&&python setup.py install
-anyenv install --init
-
-
 # Setup Zsh
 sudo apt install zsh
 
-# Powerline dependencies
-sudo apt update
-sudo apt install powerline fonts-powerline
-
-# Install latest neovim
-sudo apt remove neovim
-sudo apt-add-repository ppa:neovim-ppa/unstable
-sudo apt update
-sudo apt install neovim
-
-
-# Install nodejs for fern
-curl -fsSL https://deb.nodesource.com/setup_18.x | sudo -E bash -
-sudo apt-get install -y nodejs
 
 # Install build utilities
 sudo apt-get -y install clang llvm

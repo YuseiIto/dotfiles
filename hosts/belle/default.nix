7c36ea6ba2037a5bb900ee350fe236b6567e66f6
Yusei Ito <me@yuseiito.com>
2025-06-03 22:42:23 +0900

    fix: add stateVersion

--- hosts/belle/default.nix
+++ hosts/belle/default.nix
@@ -1,8 +1,8 @@
 { ... }:
 let username = "yuseiito";
 in {
+  system.stateVersion = 5;
   imports = [ ../../modules/darwin ];
-
   users.users."${username}" = { home = "/Users/${username}"; };
 
   networking = { hostName = "belle"; };

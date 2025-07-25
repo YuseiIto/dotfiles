333912ef0879135d51261c9f38f57653aa2cfe37
Yusei Ito <me@yuseiito.com>
2025-07-25 10:55:08 +0900

    revert: abort nixification

--- hosts/belle/default.nix
+++
@@ -1,9 +0,0 @@
-{ ... }:
-let username = "yuseiito";
-in {
-  system.stateVersion = 5;
-  imports = [ ../../modules/darwin ];
-  users.users."${username}" = { home = "/Users/${username}"; };
-
-  networking = { hostName = "belle"; };
-}

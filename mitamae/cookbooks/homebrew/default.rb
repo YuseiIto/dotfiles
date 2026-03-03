f2932506a4cd44ff0d181e19b373deea250f4158
Yusei Ito <me@yuseiito.com>
2026-03-03 22:19:30 +0900

    feat: homebrew cookbook

---
+++ mitamae/cookbooks/homebrew/default.rb
@@ -0,0 +1,6 @@
+if node[:platform] == "darwin"
+  execute "Install Homebrew" do
+    command 'NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"'
+    not_if "command -v brew"
+  end
+end

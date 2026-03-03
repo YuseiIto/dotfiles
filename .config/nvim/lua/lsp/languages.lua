c5879b3f4dd7c7ee4cde700c1fd733630879fe91
Yusei Ito <me@yuseiito.com>
2026-03-04 06:56:10 +0900

    refactor(lsp): disable formatting for ts_ls and jsonls to avoid conflicts

--- .config/nvim/lua/lsp/languages.lua
+++ .config/nvim/lua/lsp/languages.lua
@@ -2,6 +2,12 @@
 -- メジャーなものはneovim/nvim-lspconfig にあるものを使う
 -- https://github.com/neovim/nvim-lspconfig/blob/master/doc/configs.md
 
+-- Utility function to Disable formatting capability of LSP client to avoid conflicts
+local function disable_formatting(client, bufnr)
+  client.server_capabilities.documentFormattingProvider = false
+  client.server_capabilities.documentRangeFormattingProvider = false
+end
+
 -- Lua Language Server
 -- https://github.com/LuaLS/lua-language-server
 vim.lsp.enable('lua_ls')
@@ -20,14 +26,24 @@ vim.lsp.enable('terraformls')
 -- https://github.com/python-lsp/python-lsp-server
 vim.lsp.enable('pylsp')
 
-
 vim.lsp.enable('nil_ls')
 
-vim.lsp.enable('biome')
-
+-- ts_ls
+-- Disable formatting for ts_ls (Leave it to biome)
+vim.lsp.config('ts_ls', {
+  on_attach = disable_formatting,
+})
 vim.lsp.enable('ts_ls')
 
+-- jsonls
+-- Disable formatting for jsonls (Leave it to biome)
+vim.lsp.config('ts_ls', {
+  on_attach = disable_formatting,
+})
 vim.lsp.enable('jsonls')
+
+-- biome
+vim.lsp.enable('biome')
 
 -- prismals
 -- https://github.com/prisma/language-tools

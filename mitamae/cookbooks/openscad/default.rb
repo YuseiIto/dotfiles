3e79307eed878f77f2ca5712ad3beef315d70926
Yusei Ito <me@yuseiito.com>
2026-03-03 22:08:44 +0900

    feat: openscad cookbook

---
+++ mitamae/cookbooks/openscad/default.rb
@@ -0,0 +1,10 @@
+if node[:platform] == "darwin"
+  execute "install openscad via homebrew cask" do
+    command "brew install --cask openscad"
+    not_if "brew list --cask openscad"
+  end
+elsif node[:platform] == "ubuntu" || node[:platform] == "debian"
+  package "openscad" do
+    user "root"
+  end
+end

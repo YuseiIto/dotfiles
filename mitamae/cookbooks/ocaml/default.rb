1f24fadd215cee737ab9f0fdea8502b0e0902a1d
Yusei Ito <me@yuseiito.com>
2026-03-04 07:37:14 +0900

    feat: enable ocaml-lsp with ocaml cookbook

--- mitamae/cookbooks/ocaml/default.rb
+++ mitamae/cookbooks/ocaml/default.rb
@@ -6,6 +6,11 @@ if %w[ubuntu debian].include?(node[:platform])
   package 'opam' do
     user 'root'
   end
+  execute 'Install ocaml-lsp-server via opam' do
+    command 'opam install -y ocaml-lsp-server'
+    not_if 'command -v ocamllsp'
+  end
 elsif node[:platform] == 'darwin'
   package 'ocaml'
+  package 'ocaml-lsp'
 end

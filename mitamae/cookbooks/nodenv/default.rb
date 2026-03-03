197a366d476f9e14f73d36002c421234b1ef26ad
Yusei Ito <me@yuseiito.com>
2026-03-04 01:41:29 +0900

    fix: configure global nodejs

--- mitamae/cookbooks/nodenv/default.rb
+++ mitamae/cookbooks/nodenv/default.rb
@@ -1,3 +1,5 @@
+global_nodejs_version = "24.14.0"
+
 if node[:platform] == "darwin"
   package "nodenv"
 elsif node[:platform] == "ubuntu" || node[:platform] == "debian"
@@ -16,5 +18,15 @@ elsif node[:platform] == "ubuntu" || node[:platform] == "debian"
   execute "Install node-build plugin" do
     command "git clone https://github.com/nodenv/node-build.git #{nodenv_root}/plugins/node-build"
     not_if "test -d #{nodenv_root}/plugins/node-build"
+  end
+
+  # Install and set global node version
+  execute "Install and set global node version" do
+    command <<-EOS
+      eval "$(nodenv init -)"
+      nodenv install #{global_nodejs_version} -s
+      nodenv global #{global_nodejs_version}
+    EOS
+    not_if "nodenv versions | grep -q '#{global_nodejs_version}'"
   end
 end

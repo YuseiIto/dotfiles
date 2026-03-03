bf560a6e05b686bce7d8aaf1d5666a29ff8d82d0
Yusei Ito <me@yuseiito.com>
2026-03-03 20:55:00 +0900

    feat: sqlite cookbook

---
+++ mitamae/cookbooks/sqlite/default.rb
@@ -0,0 +1,8 @@
+# Install SQLite
+if node[:platform] == "ubuntu" || node[:platform] == "debian"
+  package "sqlite3" do
+    user "root"
+  end
+elsif node[:platform] == "darwin"
+  package "sqlite"
+end

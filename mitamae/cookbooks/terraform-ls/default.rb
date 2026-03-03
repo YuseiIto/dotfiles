68a0eb82f92a09504cdc0df9f1f37d6471c57a41
Yusei Ito <me@yuseiito.com>
2026-03-04 08:21:55 +0900

    style: style fix

--- mitamae/cookbooks/terraform-ls/default.rb
+++ mitamae/cookbooks/terraform-ls/default.rb
@@ -3,18 +3,21 @@ if node[:platform] == 'darwin'
 elsif %w[ubuntu debian].include?(node[:platform])
   # Official way for debian/ubuntu
   execute 'Add HashiCorp GPG key' do
-    command 'wget -O- https://apt.releases.hashicorp.com/gpg | gpg --dearmor | tee /usr/share/keyrings/hashicorp-archive-keyring.gpg'
+    command 'wget -O- https://apt.releases.hashicorp.com/gpg | gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg'
     user 'root'
     not_if 'test -f /usr/share/keyrings/hashicorp-archive-keyring.gpg'
   end
 
   execute 'Add HashiCorp repo' do
-    keyring_path = '/usr/share/keyrings/hashicorp-archive-keyring.gpg'
-    repo_url = 'https://apt.releases.hashicorp.com'
-    command "echo \"deb [signed-by=#{keyring_path}] #{repo_url} $(lsb_release -cs) main\" | tee /etc/apt/sources.list.d/hashicorp.list"
+    key = '/usr/share/keyrings/hashicorp-archive-keyring.gpg'
+    url = 'https://apt.releases.hashicorp.com'
+    codename = "$(grep -oP '(?<=UBUNTU_CODENAME=).*' /etc/os-release || lsb_release -cs)"
+    sources_list = '/etc/apt/sources.list.d/hashicorp.list'
+    command "echo \"deb [arch=$(dpkg --print-architecture) signed-by=#{key}] #{url} #{codename} main\" >> #{sources_list}"
     user 'root'
     not_if 'test -f /etc/apt/sources.list.d/hashicorp.list'
   end
+
   execute 'apt-get update' do
     user 'root'
   end

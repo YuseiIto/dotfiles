16a75b99318200c6c5e67b1c0608de74a0d3828c
Yusei Ito <me@yuseiito.com>
2026-03-04 01:29:04 +0900

    style: remove useless assignment

--- mitamae/cookbooks/cfn-lint/default.rb
+++ mitamae/cookbooks/cfn-lint/default.rb
@@ -3,7 +3,6 @@ include_recipe "../uv"
 if node[:platform] == "darwin"
   package "cfn-lint"
 elsif node[:platform] == "ubuntu" || node[:platform] == "debian"
-  home = ENV["HOME"]
   execute "Install cfn-lint via uv" do
     command "uv tool install cfn-lint"
     not_if "command -v cfn-lint"

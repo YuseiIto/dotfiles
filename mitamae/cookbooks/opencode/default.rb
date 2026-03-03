1185e8ce1e7e119c4bb72ce6b91ae1628844892c
Yusei Ito <me@yuseiito.com>
2026-03-04 05:25:32 +0900

    fix: fix use npm to install opencode

--- mitamae/cookbooks/opencode/default.rb
+++ mitamae/cookbooks/opencode/default.rb
@@ -1,8 +1,9 @@
 if node[:platform] == 'darwin'
   package 'opencode'
 elsif %w[ubuntu debian].include?(node[:platform])
-  execute 'Install opencode via official installer' do
-    command 'curl -fsSL https://opencode.ai/install.sh | sh'
+  include_recipe '../nodenv'
+  execute 'Install opencode' do
+    command 'npm install -g opencode-ai'
     not_if 'command -v opencode'
   end
 end

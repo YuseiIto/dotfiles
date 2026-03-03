2dd244a59a64643bb5f14c2073f89b5484fe655a
Yusei Ito <me@yuseiito.com>
2026-03-04 07:35:25 +0900

    feat: add rust-analyzer to rust cookbook

--- mitamae/cookbooks/rust/default.rb
+++ mitamae/cookbooks/rust/default.rb
@@ -1,8 +1,13 @@
 if node[:platform] == 'darwin'
   package 'rust'
+  package 'rust-analyzer'
 elsif %w[ubuntu debian].include?(node[:platform])
   execute 'Install Rust via rustup' do
     command "curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y"
     not_if 'command -v rustc'
+  end
+  execute 'Install rust-analyzer via rustup' do
+    command 'rustup component add rust-analyzer'
+    not_if 'command -v rust-analyzer'
   end
 end

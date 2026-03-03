d4fc3b2f0575bb4e66c5b46608bf40b66ab28b8b
Yusei Ito <me@yuseiito.com>
2026-03-03 21:01:06 +0900

    feat: nikto cookbook

---
+++ mitamae/cookbooks/nikto/default.rb
@@ -0,0 +1,8 @@
+# Install nikto - web server vulnerability scanner
+if node[:platform] == "ubuntu" || node[:platform] == "debian"
+  package "nikto" do
+    user "root"
+  end
+elsif node[:platform] == "darwin"
+  package "nikto"
+end

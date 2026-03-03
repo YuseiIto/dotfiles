f588a077c463f2cf90ea3f2aa09fc4f2a0aa40b4
Yusei Ito <me@yuseiito.com>
2026-03-03 21:59:39 +0900

    feat: feature_macos_media cookbook

---
+++ mitamae/cookbooks/feature_macos_media/default.rb
@@ -0,0 +1,15 @@
+# Media creation and playback
+if node[:platform] == "darwin"
+  casks = %w(
+    obs
+    vlc
+    krita
+  )
+
+  casks.each do |cask|
+    execute "install #{cask} via homebrew cask" do
+      command "brew install --cask #{cask}"
+      not_if "brew list --cask #{cask}"
+    end
+  end
+end

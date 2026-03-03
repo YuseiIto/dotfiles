f59057b6d4403cfbe50e6a10076ec76be80e9886
Yusei Ito <me@yuseiito.com>
2026-03-04 08:04:01 +0900

    fix: always use rustup for rust

--- mitamae/cookbooks/rust/default.rb
+++ mitamae/cookbooks/rust/default.rb
@@ -1,14 +1,11 @@
-if node[:platform] == 'darwin'
-  package 'rust'
-  package 'rust-analyzer'
-elsif %w[ubuntu debian].include?(node[:platform])
-  execute 'Install Rust via rustup' do
-    command "curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y"
-    not_if 'command -v rustc'
-  end
-  execute 'Install rust-analyzer via rustup' do
-    home = ENV['HOME']
-    command "bash -c 'source #{home}/.cargo/env && rustup component add rust-analyzer'"
-    not_if "bash -c 'source #{home}/.cargo/env && command -v rust-analyzer'"
-  end
+home = ENV['HOME']
+
+execute 'Install Rust via rustup' do
+  command "curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y"
+  not_if "test -f #{home}/.cargo/bin/rustc"
+end
+
+execute 'Install rust-analyzer via rustup' do
+  command "bash -c 'source #{home}/.cargo/env && rustup component add rust-analyzer'"
+  not_if "bash -c 'source #{home}/.cargo/env && rustup component list | grep -q \"rust-analyzer.*(installed)\"'"
 end

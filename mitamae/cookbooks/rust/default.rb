86b5c11f27608d088625fb66493bdd40f2fe8955
Yusei Ito <me@yuseiito.com>
2026-03-03 21:32:16 +0900

    feat: rust cookbook

---
+++ mitamae/cookbooks/rust/default.rb
@@ -0,0 +1,8 @@
+if node[:platform] == "darwin"
+  package "rust"
+elsif node[:platform] == "ubuntu" || node[:platform] == "debian"
+  execute "Install Rust via rustup" do
+    command "curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y"
+    not_if "command -v rustc"
+  end
+end

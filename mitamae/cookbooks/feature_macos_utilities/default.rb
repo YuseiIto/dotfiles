270d97c8e635795b354f8f987941e7fb7e68cc9a
Yusei Ito <me@yuseiito.com>
2026-03-03 21:57:12 +0900

    feat: feature_macos_utilities

---
+++ mitamae/cookbooks/feature_macos_utilities/default.rb
@@ -0,0 +1,23 @@
+# System utilities, file management, remote access
+if node[:platform] == "darwin"
+  casks = %w(
+    alacritty
+    alfred
+    appcleaner
+    cyberduck
+    keyboard-cleaner
+    keycastr
+    onyx
+    utm
+    vnc-viewer
+    raspberry-pi-imager
+    zap
+  )
+
+  casks.each do |cask|
+    execute "install #{cask} via homebrew cask" do
+      command "brew install --cask #{cask}"
+      not_if "brew list --cask #{cask}"
+    end
+  end
+end

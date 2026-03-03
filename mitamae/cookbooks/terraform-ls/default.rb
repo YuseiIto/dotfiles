3571e74af61eb734c3b9864c63fc82bcdc0c30c9
Yusei Ito <me@yuseiito.com>
2026-03-04 07:36:05 +0900

    feat: terraformls cookbook

---
+++ mitamae/cookbooks/terraform-ls/default.rb
@@ -0,0 +1,21 @@
+if node[:platform] == 'darwin'
+  package 'hashicorp/tap/terraform-ls'
+elsif %w[ubuntu debian].include?(node[:platform])
+  # Official way for debian/ubuntu
+  execute 'Add HashiCorp GPG key' do
+    command 'wget -O- https://apt.releases.hashicorp.com/gpg | gpg --dearmor | tee /usr/share/keyrings/hashicorp-archive-keyring.gpg'
+    user 'root'
+    not_if 'test -f /usr/share/keyrings/hashicorp-archive-keyring.gpg'
+  end
+  execute 'Add HashiCorp repo' do
+    command 'echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | tee /etc/apt/sources.list.d/hashicorp.list'
+    user 'root'
+    not_if 'test -f /etc/apt/sources.list.d/hashicorp.list'
+  end
+  execute 'apt-get update' do
+    user 'root'
+  end
+  package 'terraform-ls' do
+    user 'root'
+  end
+end

36eb0dbe8f120a086c80440b73394f43d1a28c1f
Yusei Ito <me@yuseiito.com>
2026-03-03 21:29:55 +0900

    feat: cfn-lint cookbook

--- mitamae/cookbooks/cfn-lint/default.rb
+++ mitamae/cookbooks/cfn-lint/default.rb
@@ -1,8 +1,10 @@
+include_recipe "../../cookbooks/uv"
+
 if node[:platform] == "darwin"
   package "cfn-lint"
 elsif node[:platform] == "ubuntu" || node[:platform] == "debian"
-  execute "Install cfn-lint via pipx" do
-    command "pipx install cfn-lint"
+  execute "Install cfn-lint via uv" do
+    command "uv tool install cfn-lint"
     not_if "command -v cfn-lint"
   end
 end

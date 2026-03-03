e3a7112a1128b94eb081091bbb243e82da3a1bb5
Yusei Ito <me@yuseiito.com>
2026-03-04 05:33:04 +0900

    fix: cloudflared installation

--- mitamae/cookbooks/cloudflared/default.rb
+++ mitamae/cookbooks/cloudflared/default.rb
@@ -1,5 +1,24 @@
 if %w[ubuntu debian].include?(node[:platform])
-  'https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-${node[:os_arch]}.deb'
+
+  arch = case node[:os_arch]
+         when 'x86_64'
+           'amd64'
+         when 'aarch64', 'arm64'
+           'arm64'
+         end
+
+  archive_url = "https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-#{arch}.deb"
+
+  execute 'Install cloudflared' do
+    user 'root'
+    command <<~EOC
+      curl -L -o /tmp/cloudflared.deb #{archive_url}
+      apt-get update
+      apt-get install -y /tmp/cloudflared.deb
+      rm /tmp/cloudflared.deb
+    EOC
+    not_if 'command -v cloudflared'
+  end
 else
   # macOS, Arch Linux
   package 'cloudflared'

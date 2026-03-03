6f51e30ca9d0ed9fc5d4b0dba6a6ce73cdfa01a6
Yusei Ito <me@yuseiito.com>
2026-03-03 21:58:30 +0900

    feat: feature_macos_communication cookbook

---
+++ mitamae/cookbooks/feature_macos_communication/default.rb
@@ -0,0 +1,15 @@
+# Chat and communication
+if node[:platform] == "darwin"
+  casks = %w(
+    slack
+    discord
+    keybase
+  )
+
+  casks.each do |cask|
+    execute "install #{cask} via homebrew cask" do
+      command "brew install --cask #{cask}"
+      not_if "brew list --cask #{cask}"
+    end
+  end
+end

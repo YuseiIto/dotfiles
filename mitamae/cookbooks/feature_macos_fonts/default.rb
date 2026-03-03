2f246d1d899c491b2f6e5ee46b642490cd1aba3b
Yusei Ito <me@yuseiito.com>
2026-03-03 21:53:04 +0900

    feat: feature_macos_fonts cookbook

---
+++ mitamae/cookbooks/feature_macos_fonts/default.rb
@@ -0,0 +1,14 @@
+# Fonts
+if node[:platform] == "darwin"
+  casks = %w(
+    font-eb-garamond
+    font-hack-nerd-font
+  )
+
+  casks.each do |cask|
+    execute "install #{cask} via homebrew cask" do
+      command "brew install --cask #{cask}"
+      not_if "brew list --cask #{cask}"
+    end
+  end
+end

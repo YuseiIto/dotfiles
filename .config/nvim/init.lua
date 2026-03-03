0ff0ff7aaf755bf80bf1617aaac3214d55fafa35
Yusei Ito <me@yuseiito.com>
2026-03-04 07:04:48 +0900

    wip: select `.lua` files by feature flag

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,10 +1,21 @@
+local features = require "features"
+
 require "base"
 require "key"      -- Key bindings
 require "plugins"  -- Load plugins
 require "color"    -- Colors
+
+if features.rich_presence then
 require "presence" -- Discord Rich presense
+end
+
+if features.lsp then
 require "lsp/lsp"  -- LSP Configurations
+end
+
+if features.lazygit then
 require "lazygit"  -- Open lazygit
+end
 
 -- Compile packer configuration file when saved
 vim.api.nvim_create_autocmd("BufWritePost", {
---
+++ .config/nvim/lua/features.lua
@@ -0,0 +1,6 @@
+local M = {}
+M.lsp = true
+M.lazygit = true
+M.ai = true
+M.rich_presence = true
+return M

3730d588a3f9331aff4b4acc1f953310e896b83b
Yusei Ito <me@yuseiito.com>
2026-03-04 00:55:49 +0900

    feat: resolve ollama installation dependency

--- mitamae/cookbooks/ollama/default.rb
+++ mitamae/cookbooks/ollama/default.rb
@@ -1,6 +1,7 @@
 if node[:platform] == "darwin"
   package "ollama"
 elsif node[:platform] == "ubuntu" || node[:platform] == "debian"
+  include_recipe "../zstd" # ollama installer depends on zstd
   execute "Install ollama via official installer" do
     command "curl -fsSL https://ollama.com/install.sh | sh"
     user "root"

8bc2e6e5701af54384b5efb0bb5684b3bb314273
Yusei Ito <me@yuseiito.com>
2026-03-04 02:42:16 +0900

    style: rubocop -a

--- mitamae/cookbooks/arduino-ide/default.rb
+++ mitamae/cookbooks/arduino-ide/default.rb
@@ -1,9 +1,8 @@
-if node[:platform] == 'darwin'
-  execute 'install arduino-ide via homebrew cask' do
-    command 'brew install --cask arduino-ide'
-    not_if 'brew list --cask arduino-ide'
-  end
-else
-  # Fail if not supported platform
-  raise "Unsupported platform: #{node[:platform]}"
+raise "Unsupported platform: #{node[:platform]}" unless node[:platform] == 'darwin'
+
+execute 'install arduino-ide via homebrew cask' do
+  command 'brew install --cask arduino-ide'
+  not_if 'brew list --cask arduino-ide'
 end
+
+# Fail if not supported platform

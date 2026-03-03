a95ba3e48599fd3c0d7c0408440f41bd2764a87f
Yusei Ito <me@yuseiito.com>
2026-03-03 22:06:30 +0900

    feat: arduino-ide cookbook

---
+++ mitamae/cookbooks/arduino-ide/default.rb
@@ -0,0 +1,9 @@
+if node[:platform] == "darwin"
+  execute "install arduino-ide via homebrew cask" do
+    command "brew install --cask arduino-ide"
+    not_if "brew list --cask arduino-ide"
+  end
+else
+  # Fail if not supported platform
+  raise "Unsupported platform: #{node[:platform]}"
+end

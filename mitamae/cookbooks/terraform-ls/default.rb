fa2a5a9d93abbcae75e18ad028de6df35b0750ee
Yusei Ito <me@yuseiito.com>
2026-03-04 08:29:46 +0900

    fix: fix terraform-ls installation

--- mitamae/cookbooks/terraform-ls/default.rb
+++ mitamae/cookbooks/terraform-ls/default.rb
@@ -1,9 +1,16 @@
 if node[:platform] == 'darwin'
   package 'hashicorp/tap/terraform-ls'
 elsif %w[ubuntu debian].include?(node[:platform])
+  package 'gnupg' do
+    user 'root'
+  end
+  package 'curl' do
+    user 'root'
+  end
+
   # Official way for debian/ubuntu
   execute 'Add HashiCorp GPG key' do
-    command 'wget -O- https://apt.releases.hashicorp.com/gpg | gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg'
+    command 'curl -fsSL https://apt.releases.hashicorp.com/gpg | gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg'
     user 'root'
     not_if 'test -f /usr/share/keyrings/hashicorp-archive-keyring.gpg'
   end

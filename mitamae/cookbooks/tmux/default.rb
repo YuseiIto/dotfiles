7ef69a674713de89c254726ff1d5a107e75348d4
Yusei Ito <me@yuseiito.com>
2026-03-04 20:54:04 +0900

    refactor(tmux): .tmux.conf before tpm install, use temp tmux session

--- mitamae/cookbooks/tmux/default.rb
+++ mitamae/cookbooks/tmux/default.rb
@@ -9,16 +9,27 @@ end
 
 home = ENV['HOME']
 
+# Place .tmux.conf before tpm install so the tmux server can load @plugin options
+dotfile '.tmux.conf' do
+  cookbook_dir File.dirname(__FILE__)
+end
+
 execute 'Install tpm' do
   command "git clone https://github.com/tmux-plugins/tpm #{home}/.tmux/plugins/tpm"
   not_if "test -d #{home}/.tmux/plugins/tpm"
 end
 
 execute 'Install tpm plugins' do
-  command "#{home}/.tmux/plugins/tpm/bin/install_plugins"
+  # install_plugins uses `tmux show-option` to read the @plugin list from the
+  # running server. Start a detached server, source the config to ensure
+  # @plugin options are loaded, install, then clean up only the session we
+  # created (leave any existing server intact for macOS bare metal).
+  command <<~EOC
+    tmux start-server
+    tmux new-session -d -s mitamae_setup 2>/dev/null || true
+    tmux source-file #{home}/.tmux.conf
+    #{home}/.tmux/plugins/tpm/bin/install_plugins
+    tmux kill-session -t mitamae_setup 2>/dev/null || true
+  EOC
   not_if "test -d #{home}/.tmux/plugins/tmux-sensible"
-end
-
-dotfile '.tmux.conf' do
-  cookbook_dir File.dirname(__FILE__)
 end

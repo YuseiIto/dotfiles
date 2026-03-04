17b88c365fdc38e8c5fae235e31cd9c10c96a13e
Yusei Ito <me@yuseiito.com>
2026-03-05 00:55:53 +0900

    fix(google-chrome): use dynamic arch for APT repository

--- mitamae/cookbooks/google-chrome/default.rb
+++ mitamae/cookbooks/google-chrome/default.rb
@@ -4,7 +4,7 @@ elsif %w[ubuntu debian].include?(node[:platform])
   execute 'Add Google Chrome APT repository' do
     command <<~EOC
       curl -fsSL https://dl.google.com/linux/linux_signing_key.pub | gpg --dearmor -o /usr/share/keyrings/google-chrome.gpg
-      echo "deb [arch=amd64 signed-by=/usr/share/keyrings/google-chrome.gpg] https://dl.google.com/linux/chrome/deb/ stable main" > /etc/apt/sources.list.d/google-chrome.list
+      echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/google-chrome.gpg] https://dl.google.com/linux/chrome/deb/ stable main" > /etc/apt/sources.list.d/google-chrome.list
       apt-get update
     EOC
     user 'root'

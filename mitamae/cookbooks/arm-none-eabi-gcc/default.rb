6e5da9cd4c6be97b9b71d73af9cf62849cdba02b
Yusei Ito <me@yuseiito.com>
2026-03-07 17:05:59 +0900

    fix: improve unsupported platform handling

--- mitamae/cookbooks/arm-none-eabi-gcc/default.rb
+++ mitamae/cookbooks/arm-none-eabi-gcc/default.rb
@@ -1,6 +1,3 @@
-if node[:platform] == 'darwin'
-  package 'arm-none-eabi-gcc'
-else
-  Mitamae.logger.error "unsupported platform #{node[:platform]}: #{__FILE__}:#{__LINE__}"
-  exit 1
-end
+raise "unsupported platform #{node[:platform]}: #{__FILE__}:#{__LINE__}" unless node[:platform] == 'darwin'
+
+package 'arm-none-eabi-gcc'

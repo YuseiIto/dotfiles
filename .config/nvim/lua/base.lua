c5d83b7fd37bfd3fc4a159fbe80ae7296f1dfa3f
Yusei Ito <me@yuseiito.com>
2026-05-10 01:30:23 +0900

    fix(nvim): showmode=false

--- .config/nvim/lua/base.lua
+++ .config/nvim/lua/base.lua
@@ -24,6 +24,7 @@ vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 vim.opt.list = true
+vim.opt.showmode = false -- Don't show the mode to avoid conflict with statusline plugins
 
 -- Remember the place of cursor
 vim.api.nvim_create_autocmd("BufReadPost", {
